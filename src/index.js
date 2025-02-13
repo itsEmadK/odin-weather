b96d4ac6f8242ee27af9387d9b776a5319f0380a
Emad <67466634+itsEmadK@users.noreply.github.com>
2025-02-13 14:30:14 +0330

    Add function to get current weather

--- src/index.js
+++ src/index.js
@@ -6,3 +6,30 @@ async function loadJson(url) {
     const json = await response.json();
     return json;
 }
+
+async function getCurrentWeather(city) {
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2CresolvedAddress%2Ctemp%2Cfeelslike%2Chumidity%2Cwindspeed%2Cuvindex%2Cconditions%2Cdescription%2Cicon&include=remote%2Cobs%2Ccurrent%2Cfcst&key=T3K39GSEC83MWTCTJQVNUSYSN&contentType=json`;
+    const weatherJson = await loadJson(url);
+    const address = weatherJson.resolvedAddress;
+    const { currentConditions } = weatherJson;
+    const {
+        temp,
+        humidity,
+        conditions,
+        feelslike: feelsLike,
+        windspeed: windSpeed,
+    } = currentConditions;
+    const currentWeather = {
+        address,
+        conditions,
+        temp,
+        feelsLike,
+        humidity,
+        windSpeed,
+    };
+    return currentWeather;
+}
+
+getCurrentWeather('semnan').then((r) => {
+    console.log(r);
+});
